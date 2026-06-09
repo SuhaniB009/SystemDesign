9307ab06f058c96e87c1cd4d9df8306adbfa8f84
Himanshi <2023ugec018@nitjsr.ac.in>
2026-06-10 01:34:02 +0530

    SimpleSingleton

---
+++ SingletonDesign Pattern/SimpleSingleton.java
@@ -0,0 +1,19 @@
+//package SingletonDesign Pattern;
+
+
+public class SimpleSingleton {
+    private static SimpleSingleton instance =null;
+    private SimpleSingleton(){
+        System.out.println("Singleton constructor called");
+    }
+    public static SimpleSingleton getInstance(){
+        if(instance == null)instance= new SimpleSingleton();
+        return instance;
+    }
+
+    public static void main(String[] args){
+        SimpleSingleton s1= SimpleSingleton.getInstance();
+        SimpleSingleton s2= SimpleSingleton.getInstance();
+        System.out.println(s1==s2);
+    }
+}
